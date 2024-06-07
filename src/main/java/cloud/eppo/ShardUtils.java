2354f774821dc9d59cae4ef8ab53b0d35bc38bf2
Felipe Lima <felipe.lima@gmail.com>
2024-06-07 14:15:41 -0700

    remove duplicated code

--- src/main/java/cloud/eppo/ShardUtils.java
+++ src/main/java/cloud/eppo/ShardUtils.java
@@ -1,9 +1,6 @@
 package cloud.eppo;
 
 import cloud.eppo.model.ShardRange;
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public final class ShardUtils {
   /** Returns whether the given shard is within the range. */
@@ -11,26 +8,8 @@ public final class ShardUtils {
     return shard >= range.getStart() && shard < range.getEnd();
   }
 
-  /** Convert input into md5 hex */
-  public static String getHex(String input) {
-    MessageDigest md;
-    try {
-      md = MessageDigest.getInstance("MD5");
-    } catch (NoSuchAlgorithmException e) {
-      throw new RuntimeException("Error computing md5 hash", e);
-    }
-    byte[] messageDigest = md.digest(input.getBytes());
-    BigInteger no = new BigInteger(1, messageDigest);
-    String hashText = no.toString(16);
-    while (hashText.length() < 32) {
-      hashText = "0" + hashText;
-    }
-
-    return hashText;
-  }
-
   public static int getShard(String input, int maxShardValue) {
-    StringBuilder hashText = new StringBuilder(getHex(input));
+    StringBuilder hashText = new StringBuilder(Utils.getMD5Hex(input));
     while (hashText.length() < 32) {
       hashText.insert(0, "0");
     }
