ab7c178b46453ceb15728e840240a1d1eedabae8
Tyler Potter <tyler.potter@geteppo.com>
2024-10-15 14:38:34 -0600

    fix: make empty config default instead of null (#51)
    * make default configuration empty set.

--- src/main/java/cloud/eppo/IConfigurationStore.java
+++ src/main/java/cloud/eppo/IConfigurationStore.java
@@ -2,13 +2,14 @@ package cloud.eppo;
 
 import cloud.eppo.api.Configuration;
 import java.util.concurrent.CompletableFuture;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Common interface for extensions of this SDK to support caching and other strategies for
  * persisting configuration data across sessions.
  */
 public interface IConfigurationStore {
-  Configuration getConfiguration();
+  @NotNull Configuration getConfiguration();
 
   CompletableFuture<Void> saveConfiguration(Configuration configuration);
 }
