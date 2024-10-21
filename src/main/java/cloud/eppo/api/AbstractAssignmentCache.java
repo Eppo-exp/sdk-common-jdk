53aba221108518d108bf2b61680aecf9fe3c09be
Ty Potter <tyler@fluxon.com>
2024-10-21 09:33:03 -0600

    minimal map

--- src/main/java/cloud/eppo/api/AbstractAssignmentCache.java
+++ src/main/java/cloud/eppo/api/AbstractAssignmentCache.java
@@ -3,16 +3,42 @@ package cloud.eppo.api;
 import cloud.eppo.cache.AssignmentCacheEntry;
 import cloud.eppo.cache.AssignmentCacheKey;
 import java.util.Map;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * {@link IAssignmentCache} implementation which takes a map to use as the underlying storage
  * mechanism.
  */
 public abstract class AbstractAssignmentCache implements IAssignmentCache {
-  protected final Map<String, String> delegate;
+
+  /** Minimal "map" implementation required to store cached assignment data. */
+  public interface CacheDelegate {
+    void put(String cacheKey, @NotNull String serializedEntry);
+
+    @Nullable String get(String cacheKey);
+  }
+
+  protected final CacheDelegate delegate;
+
+  protected AbstractAssignmentCache(final CacheDelegate delegate) {
+    this.delegate = delegate;
+  }
 
   protected AbstractAssignmentCache(final Map<String, String> delegate) {
-    this.delegate = delegate;
+    this(
+        new CacheDelegate() {
+
+          @Override
+          public void put(String cacheKey, @NotNull String serializedEntry) {
+            delegate.put(cacheKey, serializedEntry);
+          }
+
+          @Nullable @Override
+          public String get(String cacheKey) {
+            return delegate.get(cacheKey);
+          }
+        });
   }
 
   @Override
@@ -21,7 +47,7 @@ public abstract class AbstractAssignmentCache implements IAssignmentCache {
     return serializedEntry != null && serializedEntry.equals(entry.getValueKeyString());
   }
 
-  private String get(AssignmentCacheKey key) {
+  protected String get(AssignmentCacheKey key) {
     return delegate.get(key.toString());
   }
 
