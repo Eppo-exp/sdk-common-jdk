2447ba56aa277c62ffa7cfa0f70bf1ec97cf8655
Ty Potter <tyler@fluxon.com>
2025-01-24 14:52:55 -0700

    Add Fetch Configuration Task to the common sdk

---
+++ src/main/java/cloud/eppo/FetchConfigurationTask.java
@@ -0,0 +1,37 @@
+package cloud.eppo;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FetchConfigurationTask extends TimerTask {
+  private static final Logger log = LoggerFactory.getLogger(FetchConfigurationTask.class);
+  private final Runnable runnable;
+  private final Timer timer;
+  private final long intervalInMillis;
+  private final long jitterInMillis;
+
+  FetchConfigurationTask(
+      Runnable runnable, Timer timer, long intervalInMillis, long jitterInMillis) {
+    this.runnable = runnable;
+    this.timer = timer;
+    this.intervalInMillis = intervalInMillis;
+    this.jitterInMillis = jitterInMillis;
+  }
+
+  @Override
+  public void run() {
+    // TODO: retry on failed fetches
+    try {
+      runnable.run();
+    } catch (Exception e) {
+      log.error("[Eppo SDK] Error fetching experiment configuration", e);
+    }
+
+    long delay = this.intervalInMillis - (long) (Math.random() * this.jitterInMillis);
+    FetchConfigurationTask nextTask =
+        new FetchConfigurationTask(runnable, timer, intervalInMillis, jitterInMillis);
+    timer.schedule(nextTask, delay);
+  }
+}
