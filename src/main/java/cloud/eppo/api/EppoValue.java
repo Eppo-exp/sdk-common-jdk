202e4f0590fc6fb47440d6ceb5328e1fee99a956
Heath Borders <hborders@twitch.tv>
2025-05-20 16:09:13 -0500

    Updated to use new EppoValue.getType()

--- src/main/java/cloud/eppo/api/EppoValue.java
+++ src/main/java/cloud/eppo/api/EppoValue.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Objects;
 
 public class EppoValue {
-  public final EppoValueType type;
+  protected final EppoValueType type;
   protected Boolean boolValue;
   protected Double doubleValue;
   protected String stringValue;
@@ -90,6 +90,10 @@ public class EppoValue {
 
   public boolean isStringArray() {
     return type == EppoValueType.ARRAY_OF_STRING;
+  }
+
+  public EppoValueType getType() {
+    return type;
   }
 
   @Override
--- src/main/java/cloud/eppo/ufc/dto/adapters/EppoValueSerializer.java
+++ src/main/java/cloud/eppo/ufc/dto/adapters/EppoValueSerializer.java
@@ -1,6 +1,8 @@
 package cloud.eppo.ufc.dto.adapters;
 
 import cloud.eppo.api.EppoValue;
+import cloud.eppo.ufc.dto.EppoValueType;
+
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
@@ -18,23 +20,31 @@ public class EppoValueSerializer extends StdSerializer<EppoValue> {
   @Override
   public void serialize(EppoValue src, JsonGenerator jgen, SerializerProvider provider)
       throws IOException {
-    switch (src.type) {
-      case NULL:
-        jgen.writeNull();
-        break;
-      case BOOLEAN:
-        jgen.writeBoolean(src.booleanValue());
-        break;
-      case NUMBER:
-        jgen.writeNumber(src.doubleValue());
-        break;
-      case STRING:
-        jgen.writeString(src.stringValue());
-        break;
-      case ARRAY_OF_STRING:
-        String[] arr = src.stringArrayValue().toArray(new String[0]);
-        jgen.writeArray(arr, 0, arr.length);
-        break;
+    final EppoValueType type = src.getType();
+    if (type == null) {
+      // this should never happen, but if it does,
+      // we need to write something so that we're valid JSON
+      // so a null value is safest.
+      jgen.writeNull();
+    } else {
+      switch (src.getType()) {
+        case NULL:
+          jgen.writeNull();
+          break;
+        case BOOLEAN:
+          jgen.writeBoolean(src.booleanValue());
+          break;
+        case NUMBER:
+          jgen.writeNumber(src.doubleValue());
+          break;
+        case STRING:
+          jgen.writeString(src.stringValue());
+          break;
+        case ARRAY_OF_STRING:
+          String[] arr = src.stringArrayValue().toArray(new String[0]);
+          jgen.writeArray(arr, 0, arr.length);
+          break;
+      }
     }
   }
 }
