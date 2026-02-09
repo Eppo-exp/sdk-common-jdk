80d9336ebef4521edb3dcdd4e0072c20671f5223
Tyler Potter <tyler.potter@datadoghq.com>
2026-02-09 13:57:14 -0700

    feat: request, response, client, factory

---
+++ src/main/java/cloud/eppo/http/EppoConfigurationClient.java
@@ -0,0 +1,24 @@
+package cloud.eppo.http;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Interface for HTTP client implementations used by the Eppo SDK.
+ *
+ * <p>Implementations of this interface handle all HTTP communication with Eppo's servers. The SDK
+ * provides a default implementation using OkHttp in the eppo-sdk-common module, but custom
+ * implementations can be provided for specialized use cases.
+ */
+public interface EppoConfigurationClient {
+
+  /**
+   * Performs an asynchronous GET request.
+   *
+   * <p>For synchronous behavior, callers can use {@code .get()} or {@code .join()} on the returned
+   * CompletableFuture.
+   *
+   * @param request the request to execute
+   * @return a CompletableFuture that will complete with the response
+   */
+  CompletableFuture<EppoConfigurationResponse> get(EppoConfigurationRequest request);
+}
