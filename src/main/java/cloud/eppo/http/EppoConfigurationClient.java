d4161e91b883c29ba66b4d437b601abc4bcf89ba
Tyler Potter <tyler.potter@datadoghq.com>
2026-02-11 12:10:01 -0700

    lint

--- src/main/java/cloud/eppo/http/EppoConfigurationClient.java
+++ src/main/java/cloud/eppo/http/EppoConfigurationClient.java
@@ -5,11 +5,11 @@ import java.util.concurrent.CompletableFuture;
 /**
  * Defines the contract for configuration clients utilized by the Eppo SDK.
  *
- * <p>
- * Implementations of this interface are responsible for all interactions with configuration endpoint servers,
- * whether hosted by Eppo or others. The SDK includes a default implementation using OKHttp (in the eppo-sdk-common module),
- * but users can supply custom implementations to accommodate specialized needs, such as custom logging, 
- * authentication, or networking requirements.
+ * <p>Implementations of this interface are responsible for all interactions with configuration
+ * endpoint servers, whether hosted by Eppo or others. The SDK includes a default implementation
+ * using OKHttp (in the eppo-sdk-common module), but users can supply custom implementations to
+ * accommodate specialized needs, such as custom logging, authentication, or networking
+ * requirements.
  */
 public interface EppoConfigurationClient {
 
