24e4aa2e6d4ad067ee331baec39a04e5e9fcc21a
Tyler Potter <tyler.potter@datadoghq.com>
2026-08-25 22:09:59 -0600

    chore: make ConfigurationRequestor package-private, narrow requestor field visibility
    ConfigurationRequestor is an internal orchestrator; EppoConfigurationClient
    is the correct customization point for fetch behavior, not the requestor.
    
    Change requestor field from protected to package-private in BaseEppoClient
    so external subclasses cannot access it. ConfigurationRequestor reverts to
    package-private.

--- src/main/java/cloud/eppo/BaseEppoClient.java
+++ src/main/java/cloud/eppo/BaseEppoClient.java
@@ -29,7 +29,7 @@ import org.slf4j.LoggerFactory;
 public class BaseEppoClient<ConfigurationType extends SerializableEppoConfiguration, JsonFlagType> {
   private static final Logger log = LoggerFactory.getLogger(BaseEppoClient.class);
 
-  protected final ConfigurationRequestor<ConfigurationType, JsonFlagType> requestor;
+  final ConfigurationRequestor<ConfigurationType, JsonFlagType> requestor;
   private final IConfigurationStore<ConfigurationType> configurationStore;
   private final AssignmentLogger assignmentLogger;
   private final BanditLogger banditLogger;
--- src/main/java/cloud/eppo/ConfigurationRequestor.java
+++ src/main/java/cloud/eppo/ConfigurationRequestor.java
@@ -15,7 +15,7 @@ import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ConfigurationRequestor<
+class ConfigurationRequestor<
     ConfigurationType extends SerializableEppoConfiguration, JsonFlagType> {
   private static final Logger log = LoggerFactory.getLogger(ConfigurationRequestor.class);
 
