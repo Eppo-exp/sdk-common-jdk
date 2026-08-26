9a87886f883520df22b2d93b5f278643580e87ad
Tyler Potter <tyler.potter@datadoghq.com>
2026-08-25 22:04:10 -0600

    chore: restore ConfigurationRequestor to public
    Protected field in BaseEppoClient means external subclasses need to
    name the type. Extracting an interface is the correct long-term fix
    but out of scope for this PR.

--- src/main/java/cloud/eppo/ConfigurationRequestor.java
+++ src/main/java/cloud/eppo/ConfigurationRequestor.java
@@ -15,7 +15,7 @@ import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-class ConfigurationRequestor<
+public class ConfigurationRequestor<
     ConfigurationType extends SerializableEppoConfiguration, JsonFlagType> {
   private static final Logger log = LoggerFactory.getLogger(ConfigurationRequestor.class);
 
